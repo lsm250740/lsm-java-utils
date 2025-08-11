012f4d7e1955627ad01b93b4095e00d770873677
lsm <lsm250740@gmail.com>
2025-08-11 14:09:24 +0300

    Clone from template

---
+++ Implementer/src/test/java/com/lsm/template/TrmplateMainJUnit.java
@@ -0,0 +1,17 @@
+package com.lsm.template;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class TrmplateMainJUnit {
+
+	@Test
+	void testGetVersionDoesNotThrow() {
+		assertDoesNotThrow(() -> TemplateImplMain.getVersion());
+		System.out.println("Package version: " + TemplateImplMain.getVersion());
+	}
+	
+	@Test
+	void test() {
+		System.out.println("Hello from test!");
+	}
+}
